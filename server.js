3ae9544f736c4e7ebea9c79d76e7596f034877b0
DivyanshuKaushik <divyanshukaushik44@gmail.com>
2022-08-02 18:16:16 +0530

    local support

--- server.js
+++ server.js
@@ -1,43 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const path = require('path')
-// const { ApolloServer } = require("apollo-server-express");
-const { ApolloServer } = require("apollo-server-lambda");
+const { ApolloServer } = require("apollo-server-express");
+// const { ApolloServer } = require("apollo-server-lambda");
 const {
   ApolloServerPluginLandingPageGraphQLPlayground,
 } = require("apollo-server-core");
 // const serverless = require('serverless-http');
 const { connectDB } = require("./db");
 
-// async function startApolloServer() {
-//   const server = new ApolloServer({
-//     modules: [
-//       require("./modules/users"),
-//       require("./modules/events"),
-//       require("./modules/venue"),
-//       require("./modules/invite"),
-//       require("./modules/chats"),
-//     ],
-//     context: ({ req }) => ({ req }),
-//     plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
-//   });
-//   const app = express();
-//   await server.start();
-//   server.applyMiddleware({ app });
-//   app.use('/uploads',express.static(path.join(__dirname,'uploads')))
-//   require("dotenv").config();
-//   app.use(express.urlencoded({ extended : true }));
-// app.use(express.json());
-//   app.use(cors({ origin: true }));
-//   connectDB();
-//   app.listen({ port: process.env.PORT || 4000 }, () => {
-//     console.log("Server up");
-//   });
-// }
-// startApolloServer();
-// export serverless app for AWS Lambda
-// module.exports.handler = serverless(startApolloServer);
-
+async function startApolloServer() {
   const server = new ApolloServer({
     modules: [
       require("./modules/users"),
@@ -49,15 +21,43 @@ const { connectDB } = require("./db");
     context: ({ req }) => ({ req }),
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
   });
+  const app = express();
+  await server.start();
+  server.applyMiddleware({ app });
+  app.use('/uploads',express.static(path.join(__dirname,'uploads')))
+  require("dotenv").config();
+  app.use(express.urlencoded({ extended : true }));
+app.use(express.json());
+  app.use(cors({ origin: true }));
+  connectDB();
+  app.listen({ port: process.env.PORT || 4000 }, () => {
+    console.log(`Server up at http://localhost:${process.env.PORT || 4000}/graphql`);
+  });
+}
+startApolloServer();
+// export serverless app for AWS Lambda
+// module.exports.handler = serverless(startApolloServer);
+
+  // const server = new ApolloServer({
+  //   modules: [
+  //     require("./modules/users"),
+  //     require("./modules/events"),
+  //     require("./modules/venue"),
+  //     require("./modules/invite"),
+  //     require("./modules/chats"),
+  //   ],
+  //   context: ({ req }) => ({ req }),
+  //   plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  // });
   // const app = express();
 //   server.applyMiddleware({ app });
 //   app.use('/uploads',express.static(path.join(__dirname,'uploads')))
-  require("dotenv").config();
+  // require("dotenv").config();
 //   app.use(express.urlencoded({ extended : true }));
 // app.use(express.json());
 //   app.use(cors({ origin: true }));
-  connectDB();
+  // connectDB();
 
-  module.exports.handler = server.createHandler()
+  // module.exports.handler = server.createHandler()
 
  
